9115e67d40c4392218eb46dff9d3251260517661
self <tanushupsc.09@gmail.com>
2026-03-12 17:43:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+import { butterfliesBackground } from 'https://unpkg.com/threejs-toys@0.0.7/build/threejs-toys.module.cdn.min.js'
+
+butterfliesBackground({
+  el: document.getElementById('app'),
+  eventsEl: document.body
+});
+
 const params = new URLSearchParams(window.location.search);
 const id = params.get("id");
 
