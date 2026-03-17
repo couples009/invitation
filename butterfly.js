187f898dbe4add4e7da542c6d3bb73160765837e
self <tanushupsc.09@gmail.com>
2026-03-17 12:13:45 +0530

    Update butterfly.js

--- butterfly.js
+++ butterfly.js
@@ -1,23 +1,25 @@
-import { butterfliesBackground } from 'https://unpkg.com/threejs-toys@0.0.7/build/threejs-toys.module.cdn.min.js'
+import { butterfliesBackground } from 'https://unpkg.com/threejs-toys@0.0.7/build/threejs-toys.module.cdn.min.js';
 
-const pc = butterfliesBackground({
-	el: document.getElementById('app'),
-	eventsEl: document.getElementById('app'),
-  gpgpuSize: isMobile ? 18 : 42, // 🔥 reduce load
-	background: 0xffffff,
-	material: 'basic',
-	materialParams: { transparent: true, alphaTest: 0.5 },
-	texture: 'https://assets.codepen.io/33787/butterflies.png',
-	textureCount: 4,
-	wingsScale:  isMobile ? [0.6,0.6,0.6] : [1,1,1],
-	wingsWidthSegments: 8,
-	wingsHeightSegments: 8,
-	wingsSpeed: 0.75,
-	wingsDisplacementScale: 1.25,
-	noiseCoordScale: 0.01,
-	noiseTimeCoef: 0.0005,
-	noiseIntensity: 0.0025,
-	attractionRadius1: 100,
-	attractionRadius2: 150,
-	maxVelocity: isMobile ? 0.05 : 0.1
+window.addEventListener("load", () => {
+
+  const isMobile = window.innerWidth < 768;
+
+  butterfliesBackground({
+    el: document.getElementById('app'),
+    eventsEl: document.getElementById('app'),
+
+    gpgpuSize: isMobile ? 18 : 42,
+
+    background: 0xffffff,
+    material: 'basic',
+    materialParams: { transparent: true, alphaTest: 0.5 },
+
+    texture: 'https://assets.codepen.io/33787/butterflies.png',
+    textureCount: 4,
+
+    wingsScale: isMobile ? [0.6,0.6,0.6] : [1,1,1],
+
+    maxVelocity: isMobile ? 0.05 : 0.1
+  });
+
 });
